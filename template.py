d0544d7f31bd7d98d4a01fdabf3ec6cabd00b8be
SirJaydenu <111903294+SirJaydenu@users.noreply.github.com>
2023-02-05 01:15:16 -0600

    COMPLETED

--- template.py
+++ template.py
@@ -16,34 +16,29 @@ def get_data_list(FILE_NAME):
 def get_monthly_averages(data_list):
 	del data_list[0]
 	monthAvgList = []
-	avg = []
-	y=0
+	month = data_list[0][0][:7]
+	denomSum = []
+	numerTotalSales = []
 	for a in data_list:
-		#a is the row x should be the next row
-		print(index(a))
-		date = a[0][:7]
-		currdate = x[0][:5]
-		v1 = a[5]
-		c1 = a[6]
-		v2 = x[5]
-		c2 = x[6]
-		print(" v1: "+v1+" c1: " + c1 +" v2: "+v2+" c2: "+c2)
-		if currdate == date:
-			tuple.append(((v1 * c1 + v2 + c2) / (v1 + v2)))
-		elif currdate == date and currdate != year:
-			tuple.append(((v1*c1) / v1))
+		if a[0][:7] == month:
+			denomSum.append(float(a[5]))
+			numerTotalSales.append(float(a[5])*float(a[6]))
 		else:
-			print("woo")
-		monthAvgList.append(tuple())
-		y+=2
+			averagePrice = sum(numerTotalSales)/sum(denomSum)
+			month = a[0][:7]
+			denomSum = []
+			numerTotalSales = []
+			monthAvgList.append(averagePrice)
 	return monthAvgList
 #name: print_info
 #param: monthly_average_list <list> - the list that you will process
 #brief: print the top 6 and bottom 6 months for Google stock
 #return: None
 def print_info(monthly_average_list):
-	print("Top 6 Best Months for Google Stock: " , monthly_average_list.sort())
-	print("Bottom 6 Worse Months for Google Stock: " , (monthly_average_list.reverse()))
+	monthly_average_list.sort(reverse= True)
+	print("Top 6 Best Months for Google Stock: " , monthly_average_list[:6])
+	monthly_average_list.sort(reverse = True)
+	print("Bottom 6 Worse Months for Google Stock: " , (monthly_average_list[:-6:-1]))
 print_info(get_monthly_averages((get_data_list("table.csv"))))
 # call get_data_list function to get the data list, save the return in data_list
 
